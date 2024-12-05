049931fb6b35e62bcf689a3514d4e5356d874ace
Mahmudul Hasan Sazim <159936624+MHSazim@users.noreply.github.com>
2024-12-05 10:25:33 +0600

    Add files via upload

---
+++ number.js
@@ -0,0 +1,24 @@
+// Numbers:
+let  l = 16;
+
+let weight = 7.5;
+
+let x=l + 12;
+document.getElementById("demo").innerHTML = x;
+
+// // Strings:
+// let color = "Yellow";
+// let lastName = "Johnson";
+
+// // Booleans
+// let x = true;
+// let y = false;
+
+// // Object:
+// const person = {firstName:"John", lastName:"Doe"};
+
+// // Array object:
+// const cars = ["Saab", "Volvo", "BMW"];
+
+// // Date object:
+// const date = new Date("2022-03-25");
